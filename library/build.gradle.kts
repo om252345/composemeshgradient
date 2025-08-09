f0c4460b58a9cc79ece5d963ebde175cb619dacd
omkardeshmukh <omkard@gmail.com>
2025-08-10 00:21:39 +0530

    added github action for maven release.

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("maven-publish")
+    alias(libs.plugins.vanniktech.maven.publish)
 }
 
 group = "io.github.om252345"
@@ -51,46 +51,33 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            afterEvaluate {
-                from(components["release"])
-            }
-            groupId = project.group.toString()
-            artifactId = "composemeshgradient"
-            version = project.version.toString()
 
-            pom {
-                name.set("Compose Mesh Gradient")
-                description.set("An Android Compose library for mesh gradients.")
-                url.set("https://github.com/om252345/composemeshgradient")
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("om252345")
-                        name.set("Omkar Deshmukh")
-                        email.set("omkard@gmail.com")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:github.com/om252345/composemeshgradient.git")
-                    developerConnection.set("scm:git:ssh://github.com/om252345/composemeshgradient.git")
-                    url.set("https://github.com/om252345/composemeshgradient")
-                }
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates(group.toString(), "composemeshgradient", version.toString())
+
+    pom {
+        name.set("Compose Mesh Gradient")
+        description.set("An Android Compose library for mesh gradients.")
+        url.set("https://github.com/om252345/composemeshgradient")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
             }
         }
-    }
-    repositories {
-        maven {
-            name = "Local"
-            url = uri(layout.buildDirectory.dir("repo"))
-            // For remote (Maven Central), add credentials & correct url as needed
+        developers {
+            developer {
+                id.set("om252345")
+                name.set("Omkar Deshmukh")
+                email.set("omkard@gmail.com")
+            }
+        }
+        scm {
+            connection.set("scm:git:github.com/om252345/composemeshgradient.git")
+            developerConnection.set("scm:git:ssh://github.com/om252345/composemeshgradient.git")
+            url.set("https://github.com/om252345/composemeshgradient")
         }
     }
 }
